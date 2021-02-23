1e9bd161992a50275910cefc9f7c5aa5b6c0e559
liuwei <liuwei@wondersgroup.com>
2021-02-23 18:18:56 +0800

    add echarts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+/**
+ * 基于bootstrap2
+ */
+
+var timerSave = 1000; // 定时器
+var stopsave = 0; // 最后一次暂存标志
+var startdrag = 0; // 拖拽标志位：0-释放拖拽，1-拖拽中
+var demoHtml = $(".demo").html(); // 右侧容器html代码
+var currenteditor = null; // 当前被编辑元素的编辑器
+var layouthistory = null;  // 历史暂存数据
+var echartsObjs = {};
+
+/**
+ * 判断是否开启本地暂存功能
+ */
 function supportstorage() {
 	if (typeof window.localStorage=='object') 
 		return true;
@@ -5,6 +20,9 @@ function supportstorage() {
 		return false;		
 }
 
+/**
+ * 暂存布局代码
+ */
 function handleSaveLayout() {
 	var e = $(".demo").html();
 	if (!stopsave && e != window.demoHtml) {
@@ -15,7 +33,9 @@ function handleSaveLayout() {
 	}
 }
 
-var layouthistory; 
+/**
+ * 暂存布局代码
+ */
 function saveLayout(){
 	var data = layouthistory;
 	if (!data) {
@@ -44,6 +64,9 @@ function saveLayout(){
 	});*/
 }
 
+/**
+ * 下载布局代码
+ */
 function downloadLayout(){
 	
 	$.ajax({  
@@ -63,6 +86,9 @@ function downloadHtmlLayout(){
 	});
 }
 
+/**
+ * 撤销，返回上次暂存的内容
+ */
 function undoLayout() {
 	var data = layouthistory;
 	//console.log(data);
@@ -87,6 +113,9 @@ function undoLayout() {
 	});*/
 }
 
+/**
+ * 重做，
+ */
 function redoLayout() {
 	var data = layouthistory;
 	if (data) {
@@ -116,8 +145,18 @@ function handleJsIds() {
 	handleModalIds();
 	handleAccordionIds();
 	handleCarouselIds();
-	handleTabsIds()
+	handleTabsIds();
+	handleEchartsIds();
 }
+function randomNumber() {
+	return randomFromInterval(1, 1e6)
+}
+function randomFromInterval(e, t) {
+	return Math.floor(Math.random() * (t - e + 1) + e)
+}
+/**
+ * 设置交互组件-手风琴的id
+ */
 function handleAccordionIds() {
 	var e = $(".demo #myAccordion");
 	var t = randomNumber();
@@ -135,6 +174,9 @@ function handleAccordionIds() {
 		})
 	})
 }
+/**
+ * 设置交互组件-轮播图的id
+ */
 function handleCarouselIds() {
 	var e = $(".demo #myCarousel");
 	var t = randomNumber();
@@ -146,6 +188,9 @@ function handleCarouselIds() {
 	e.find(".left").attr("href", "#" + n);
 	e.find(".right").attr("href", "#" + n)
 }
+/**
+ * 设置交互组件-遮罩窗体的id
+ */
 function handleModalIds() {
 	var e = $(".demo #myModalLink");
 	var t = randomNumber();
@@ -155,6 +200,9 @@ function handleModalIds() {
 	e.attr("href", "#" + n);
 	e.next().attr("id", n)
 }
+/**
+ * 设置交互组件-切换卡 栏目的id
+ */
 function handleTabsIds() {
 	var e = $(".demo #myTabs");
 	var t = randomNumber();
@@ -167,12 +215,30 @@ function handleTabsIds() {
 		$(t).parent().parent().find("a[href=#" + n + "]").attr("href", "#" + r)
 	})
 }
-function randomNumber() {
-	return randomFromInterval(1, 1e6)
+/**
+ * 设置Echarts组件-所有子组件id
+ */
+function handleEchartsIds() {
+	var e = $(".demo #myEchartsDemo");
+	var t = randomNumber();
+	var r = "echarts_" + t;
+	e.attr("id", r);
+	var dom = document.getElementById(r);
+	if(dom) {
+		e.prev().remove();
+		// 基于准备好的dom，初始化echarts实例
+		var myChart = echarts.init(dom);
+		echartsObjs[r] = myChart;
+		// 指定图表的配置项和数据
+		var option = JSON.parse(e.attr("option"));
+		// 使用刚指定的配置项和数据显示图表。
+		myChart.setOption(option);
+	}
 }
-function randomFromInterval(e, t) {
-	return Math.floor(Math.random() * (t - e + 1) + e)
-}
+
+/**
+ * 注册左侧树-栅栏布局-input框改变事件
+ */
 function gridSystemGenerator() {
 	$(".lyrow .preview input").bind("keyup", function() {
 		var e = 0;
@@ -190,37 +256,32 @@ function gridSystemGenerator() {
 		}
 	})
 }
-function configurationElm(e, t) {
-	$(".demo").delegate(".configuration > a", "click", function(e) {
-		e.preventDefault();
-		var t = $(this).parent().next().next().children();
-		$(this).toggleClass("active");
-		t.toggleClass($(this).attr("rel"))
-	});
-	$(".demo").delegate(".configuration .dropdown-menu a", "click", function(e) {
-		e.preventDefault();
-		var t = $(this).parent().parent();
-		var n = t.parent().parent().next().next().children();
-		t.find("li").removeClass("active");
-		$(this).parent().addClass("active");
-		var r = "";
-		t.find("a").each(function() {
-			r += $(this).attr("rel") + " "
-		});
-		t.parent().removeClass("open");
-		n.removeClass(r);
-		n.addClass($(this).attr("rel"))
-	})
-}
+
+/**
+ * 删除元素
+ */
 function removeElm() {
 	$(".demo").delegate(".remove", "click", function(e) {
 		e.preventDefault();
-		$(this).parent().remove();
+		var parent = $(this).parent();
+		// 移除echarts组件
+		var echarts = parent.find(".echarts-demo").attr("id");
+		if(echarts && echartsObjs[echarts]){
+			echartsObjs[echarts].clear();
+			delete echartsObjs[echarts];
+		}
+		
+		// 移除整个组件容器
+		parent.remove();
+
 		if (!$(".demo .lyrow").length > 0) {
 			clearDemo()
 		}
 	})
 }
+/**
+ * 清空元素
+ */
 function clearDemo() {
 	$(".demo").empty();
 	layouthistory = null;
@@ -236,6 +297,9 @@ function changeStructure(e, t) {
 function cleanHtml(e) {
 	$(e).parent().append($(e).children().html())
 }
+/**
+ * 内容区-源码生成
+ */
 function downloadLayoutSrc() {
 	var e = "";
 	$("#download-layout").children().html($(".demo").html());
@@ -288,17 +352,14 @@ function downloadLayoutSrc() {
 	$("#downloadModal textarea").val(formatSrc)
 }
 
-var currentDocument = null;
-var timerSave = 1000;
-var stopsave = 0;
-var startdrag = 0;
-var demoHtml = $(".demo").html();
-var currenteditor = null;
+
 $(window).resize(function() {
 	$("body").css("min-height", $(window).height() - 90);
 	$(".demo").css("min-height", $(window).height() - 160)
 });
-
+/**
+ * 刷新页面后从storage重新加载数据
+ */
 function restoreData(){
 	if (supportstorage()) {
 		layouthistory = JSON.parse(localStorage.getItem("layoutdata"));
@@ -307,7 +368,36 @@ function restoreData(){
 		if (window.demoHtml) $(".demo").html(window.demoHtml);
 	}
 }
-
+/**
+ * 注册组件功能事件
+ * @param {*} e 
+ * @param {*} t 
+ */
+function configurationElm(e, t) {
+	$(".demo").delegate(".configuration > a", "click", function(e) {
+		e.preventDefault();
+		var t = $(this).parent().next().next().children();
+		$(this).toggleClass("active");
+		t.toggleClass($(this).attr("rel"))
+	});
+	$(".demo").delegate(".configuration .dropdown-menu a", "click", function(e) {
+		e.preventDefault();
+		var t = $(this).parent().parent();
+		var n = t.parent().parent().next().next().children();
+		t.find("li").removeClass("active");
+		$(this).parent().addClass("active");
+		var r = "";
+		t.find("a").each(function() {
+			r += $(this).attr("rel") + " "
+		});
+		t.parent().removeClass("open");
+		n.removeClass(r);
+		n.addClass($(this).attr("rel"))
+	})
+}
+/**
+ * 初始化右侧容器事件，拖放排序、功能事件
+ */
 function initContainer(){
 	$(".demo, .demo .column").sortable({
 		connectWith: ".column",
@@ -322,18 +412,28 @@ function initContainer(){
 			startdrag = 0;
 		}
 	});
+	// 注册组件功能事件
 	configurationElm();
 }
+
+
 $(document).ready(function() {
-	CKEDITOR.disableAutoInline = true;
+	// 先从浏览器缓存中拿上一次更新的数据，如果存在
 	restoreData();
+	// 设置ckeditor编辑器
+	CKEDITOR.disableAutoInline = true;
 	var contenthandle = CKEDITOR.replace( 'contenteditor' ,{
 		language: 'zh-cn',
 		contentsCss: ['css/bootstrap-combined.min.css'],
 		allowedContent: true
 	});
+	// 设置起始高度
 	$("body").css("min-height", $(window).height() - 90);
 	$(".demo").css("min-height", $(window).height() - 160);
+	
+	/**
+	 * 左侧树子节点-栅栏布局-拖拽事件
+	 */
 	$(".sidebar-nav .lyrow").draggable({
 		connectToSortable: ".demo",
 		helper: "clone",
@@ -362,6 +462,9 @@ $(document).ready(function() {
 			startdrag = 0;
 		}
 	});
+	/**
+	 * 左侧树子节点-其他-拖拽事件
+	 */
 	$(".sidebar-nav .box").draggable({
 		connectToSortable: ".column",
 		helper: "clone",
@@ -379,33 +482,49 @@ $(document).ready(function() {
 			startdrag = 0;
 		}
 	});
+
+	// 右侧容器事件初始化
 	initContainer();
+	/**
+	 * ckeditor编辑按钮被按下
+	 */
 	$('body.edit .demo').on("click","[data-target=#editorModal]",function(e) {
 		e.preventDefault();
 		currenteditor = $(this).parent().parent().find('.view');
 		var eText = currenteditor.html();
 		contenthandle.setData(eText);
 	});
+	/**
+	 * ckeditor编辑器弹出时的保存按钮
+	 */
 	$("#savecontent").click(function(e) {
 		e.preventDefault();
 		currenteditor.html(contenthandle.getData());
 	});
+	/**
+	 * 导航栏-下载
+	 */
 	$("[data-target=#downloadModal]").click(function(e) {
 		e.preventDefault();
 		downloadLayoutSrc();
 	});
+	/**
+	 * 导航栏-保存
+	 */
 	$("[data-target=#shareModal]").click(function(e) {
 		e.preventDefault();
 		handleSaveLayout();
 	});
-	$("#download").click(function() {
-		downloadLayout();
-		return false
-	});
+	/**
+	 * 下载弹出框被点击
+	 */
 	$("#downloadhtml").click(function() {
 		downloadHtmlLayout();
 		return false
 	});
+	/**
+	 * 导航栏-编辑
+	 */
 	$("#edit").click(function() {
 		$("body").removeClass("devpreview sourcepreview");
 		$("body").addClass("edit");
@@ -413,10 +532,16 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
+	/**
+	 * 导航栏-清空
+	 */
 	$("#clear").click(function(e) {
 		e.preventDefault();
 		clearDemo()
 	});
+	/**
+	 * 导航栏-布局编辑
+	 */
 	$("#devpreview").click(function() {
 		$("body").removeClass("edit sourcepreview");
 		$("body").addClass("devpreview");
@@ -424,6 +549,9 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
+	/**
+	 * 导航栏-预览
+	 */
 	$("#sourcepreview").click(function() {
 		$("body").removeClass("edit");
 		$("body").addClass("devpreview sourcepreview");
@@ -431,6 +559,9 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		return false
 	});
+	/**
+	 * 下载按钮点击弹出层的“自适应宽度”按钮被按下事件
+	 */
 	$("#fluidPage").click(function(e) {
 		e.preventDefault();
 		changeStructure("container", "container-fluid");
@@ -438,6 +569,9 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		downloadLayoutSrc()
 	});
+	/**
+	 * 下载按钮点击弹出层的“固定宽度”按钮被按下事件
+	 */
 	$("#fixedPage").click(function(e) {
 		e.preventDefault();
 		changeStructure("container-fluid", "container");
@@ -445,22 +579,34 @@ $(document).ready(function() {
 		$(this).addClass("active");
 		downloadLayoutSrc()
 	});
+	/**
+	 * 左侧树父节点点击事件
+	 */
 	$(".nav-header").click(function() {
 		$(".sidebar-nav .boxes, .sidebar-nav .rows").hide();
 		$(this).next().slideDown()
 	});
+	/**
+	 * 导航栏-撤销
+	 */
 	$('#undo').click(function(){
 		stopsave++;
 		if (undoLayout()) initContainer();
 		stopsave--;
 	});
+	/**
+	 * 导航栏-重做
+	 */
 	$('#redo').click(function(){
 		stopsave++;
 		if (redoLayout()) initContainer();
 		stopsave--;
 	});
+	// 注册删除元素事件
 	removeElm();
+	// 注册左侧树-栅栏布局-input框改变事件
 	gridSystemGenerator();
+	// 每隔1s暂存一次
 	setInterval(function() {
 		handleSaveLayout()
 	}, timerSave)
